7557f4970c59c5346c3edf81e73bb8a80fadc176
charliebr0wn <cdelaorden@gmail.com>
2016-05-26 12:17:04 +0200

    Fix app.js error in chapter 5

--- ejercicios/tema5/src/app.js
+++ ejercicios/tema5/src/app.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-<<<<<<< HEAD
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from './configureStore';
 import Ecommerce from './components/ecommerce';
 
-// importar browserHistory de react-router y pasarlo como
-// prop a Ecommerce
-import { browserHistory } from 'react-router';
 
 const store = configureStore();
 //TODO - ejercicio
